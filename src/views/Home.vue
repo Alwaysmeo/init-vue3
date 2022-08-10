983f1704824684b06dac092d31a99165835dfa74
always <hello2000@vip.qq.com>
2022-08-10 09:14:44 +0800

    🚩 完善图片裁剪插件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,21 @@
 <template>
     <div>
-        <PictureCropper></PictureCropper>
+        <PictureCropper :modelVisible="state.modelVisible" :aspectRatio="1 / 2" @onSuccess="PictureCropperSuccess"></PictureCropper>
     </div>
 </template>
 
 <script setup>
+import { reactive } from 'vue'
 import { PictureCropper } from '@components'
+
+const state = reactive({
+    modelVisible: true,
+})
+
+const PictureCropperSuccess = (url) => {
+    state.modelVisible = false
+    console.log(url)
+}
 </script>
 
 <style scoped></style>
